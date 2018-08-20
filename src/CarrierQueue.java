fb6edaeb2151976c7a65c584bb1c000046227bb4
Adriano <adriano.shimonoe@gmail.com>
2018-08-20 17:10:07 -0300

    Remoção da fila baseado na prioridade.

--- src/CarrierQueue.java
+++ src/CarrierQueue.java
@@ -7,12 +7,14 @@ import org.apache.zookeeper.data.Stat;
 import java.io.UnsupportedEncodingException;
 import java.nio.ByteBuffer;
 import java.nio.charset.StandardCharsets;
+import java.util.List;
 
 import static java.nio.ByteBuffer.allocate;
 
 public class CarrierQueue extends SyncPrimitive.Queue{
 
-    String root;
+    private String root;
+    private static Integer mutex = -1;
 
     /**
      * Constructor of producer-consumer queue
@@ -70,4 +72,48 @@ public class CarrierQueue extends SyncPrimitive.Queue{
         }
         return false;
     }
+
+    public String get() throws KeeperException, InterruptedException {
+        String retvalue;
+        Stat stat = null;
+
+        synchronized (mutex) {
+            List<String> priorityQueue = zk.getChildren(this.root + "priorityQueue", true);
+            List<String> normalQueue = zk.getChildren(this.root + "normalQueue", true);
+            if (priorityQueue.size() == 0 || normalQueue.size() == 0) {
+                System.out.println("Empty queue. Going to wait");
+                mutex.wait();
+            } else {
+                List<String> list;
+                String queueType;
+                if (priorityQueue.size() > 0) {
+                    list = priorityQueue;
+                    queueType = "priorityQueue/";
+                } else {
+                    list = normalQueue;
+                    queueType = "normalQueue/";
+                }
+
+                Integer min = new Integer(list.get(0).substring(7));
+                System.out.println("List: " + list.toString());
+                String minString = list.get(0);
+                for (String s : list) {
+                    Integer tempValue = new Integer(s.substring(7));
+                    //System.out.println("Temp value: " + tempValue);
+                    if (tempValue < min) {
+                        min = tempValue;
+                        minString = s;
+                    }
+               }
+                byte[] b = zk.getData(this.root + "/" + queueType + minString, false, stat);
+                //System.out.println("b: " + Arrays.toString(b));
+                zk.delete(root + "/" + minString, 0);
+                ByteBuffer buffer = ByteBuffer.wrap(b);
+                retvalue = String.valueOf(buffer);
+                return retvalue;
+            }
+        }
+        return null;
+    }
+
 }
