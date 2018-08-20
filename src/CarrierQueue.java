d08c661f2339c68b98e3e6c4770d2336cc2e0764
Adriano <adriano.shimonoe@gmail.com>
2018-08-20 16:05:15 -0300

    Código adaptado para o ZK baseado no SyncPrimitive, usando as premissas de dois tipos de filas.

--- src/CarrierQueue.java
+++ src/CarrierQueue.java
@@ -1,30 +1,73 @@
-import java.util.PriorityQueue;
-import java.util.Queue;
-import java.util.concurrent.SynchronousQueue;
+import org.apache.zookeeper.CreateMode;
+import org.apache.zookeeper.KeeperException;
+import org.apache.zookeeper.ZooDefs;
+import org.apache.zookeeper.ZooKeeper;
+import org.apache.zookeeper.data.Stat;
 
-public class CarrierQueue {
+import java.io.UnsupportedEncodingException;
+import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
 
-    Queue priorityQueue;
-    Queue normalQueue;
+import static java.nio.ByteBuffer.allocate;
 
-    public CarrierQueue() {
-        priorityQueue = new PriorityQueue();
-        normalQueue = new SynchronousQueue();
+public class CarrierQueue extends SyncPrimitive.Queue{
+
+    String root;
+
+    /**
+     * Constructor of producer-consumer queue
+     *
+     * @param address
+     * @param name
+     */
+    CarrierQueue(String address, String name, ZooKeeper zk) {
+        super(address, name);
+        this.root = name;
+
+        createQueue(root, "priority");
+        createQueue(root, "normal");
     }
 
-    public void addToPriorityQueue(String packageDescription) {
-        this.priorityQueue.add(packageDescription);
+    /**
+     * Queue creator method
+     *
+     * @param path: indicates the path of data on zk server
+     * @param type: "priority" for priority queue or "normal" for normal queue
+     */
+    private boolean createQueue(String path, String type) {
+        if (type.equals("priority") || type.equals("normal")) {
+            if (zk != null) {
+                try {
+                    Stat s = zk.exists(path, false);
+                    if (s == null) {
+                        zk.create(path + type + "Queue/", new byte[0], ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT_SEQUENTIAL);
+                    }
+                } catch (InterruptedException | KeeperException e) {
+                    e.printStackTrace();
+                }
+            }
+            return true;
+        }
+        return false;
     }
 
-    public void addToNormalQueue(String packageDescription) {
-        this.normalQueue.add(packageDescription);
-    }
+    /**
+     * Constructor of producer-consumer queue
+     *
+     * @param queueType: To insert into priority queue or normal queue
+     * @param message: The message to store in
+     */
+    public boolean add(String queueType, String message) throws KeeperException, InterruptedException {
+        if (queueType.equals("priority") || queueType.equals("normal")) {
+            ByteBuffer b = null;
+            b = ByteBuffer.wrap(message.getBytes(StandardCharsets.UTF_8));
 
-    public String getFromPriorityQueue() {
-        return String.valueOf(this.priorityQueue.poll());
-    }
+            assert b != null;
+            byte[] value = b.array();
+            zk.create(root + queueType + "/Queue", value, ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT_SEQUENTIAL);
 
-    public String getFromNormalQueue() {
-        return String.valueOf(this.normalQueue.poll());
+            return true;
+        }
+        return false;
     }
 }
